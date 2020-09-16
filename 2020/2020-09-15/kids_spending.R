e2811a5c7d60bb3a22669830cf2b28237ffa0294
Matt Keefe <matthew.j.keefe@disney.com>
2020-09-16 16:37:34 -0400

    adding kids spending tidytuesday

--- 2020/2020-09-15/kids_spending.R
+++ 2020/2020-09-15/kids_spending.R
@@ -8,6 +8,25 @@
 library(tidytuesdayR)
 library(tidyverse)
 library(ggrepel)
+library(gridExtra)
+library(grid)
+library(png)
+library(RCurl)
+
+# Helper Functions --------------------------------------------------------
+mypng <- function(x
+                  , type='cairo-png'
+                  , width=2.2*(40/3)/2
+                  , height=2.2*(7.5)/2
+                  , units='in'
+                  , res=300){
+  png(x
+      , type='cairo-png'
+      , width=width
+      , height=height
+      , units=units
+      , res=res)
+}
 
 # Read in data ------------------------------------------------------------
 
@@ -30,8 +49,9 @@ kids_dat %>%
   scale_x_continuous(breaks = 1997:2016) +
   scale_y_continuous(label = scales::dollar)
 
+fl_img <- rasterGrob(readPNG(getURLContent("https://static.wixstatic.com/media/110350_99b3565e47014b98b5ae79dc37ea00ba~mv2.png/v1/fit/w_1000,h_665,al_c,q_80/file.png")))
 
-kids_dat %>% 
+p1 <- kids_dat %>% 
   ungroup() %>% 
   filter(state=='Florida') %>% 
   group_by(variable,state) %>% 
@@ -41,6 +61,7 @@ kids_dat %>%
   ungroup() %>% 
   ggplot(aes(y=fct_reorder(variable, raw),x=raw)) +
   geom_col(fill = 'orange') +
+  annotation_custom(fl_img, xmin=200000000, xmax=350000000, ymin=1, ymax=20) +
   labs(x='Total spending',
        y='',
        title = 'Florida total spending by category 1997-2016') +
@@ -50,7 +71,7 @@ kids_dat %>%
         panel.grid.minor = element_blank(),
         plot.title.position = 'plot')
 
-kids_dat %>% 
+p2 <- kids_dat %>% 
   mutate(label = if_else(year == max(year), as.character(variable), NA_character_)) %>% 
   filter(state=='Florida', variable =='PK12ed') %>% 
   ggplot(aes(x=year,y=inf_adj_perchild)) +
@@ -65,3 +86,10 @@ kids_dat %>%
   theme(text = element_text(size=16),
         panel.grid.minor = element_blank(),
         plot.title.position = 'plot')
+
+mypng('~/Repositories/personal/tidytuesday/2020/2020-09-15/florida_kid_spending.png')
+gridExtra::grid.arrange(p1,p2, 
+             ncol=2,
+             top = textGrob("Florida Spending on Kids\n", gp=gpar(fontsize=26,font=3, col = 'orange')))
+dev.off()
+
